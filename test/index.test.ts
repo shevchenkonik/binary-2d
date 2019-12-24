cbf638cdd468deff58ee55d043e684f7d64ea61b
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-12-24 11:45:47 +0300

    Test of canvas

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,31 @@
-describe("Default", () => {
-  it("Correct", () => {})
+import Binary2D from '../src'
+
+beforeEach(() => {
+  let createElement = document.createElement.bind(document);
+  document.createElement = (tagName: string) => {
+    if (tagName === 'canvas') {
+      return {
+        getContext: () => ({}),
+        measureText: () => ({}),
+        localName: 'canvas',
+        width: 2,
+        height: 2,
+        data: [
+          0, 255, 255, 255,
+          0, 255, 255, 255,
+          0, 0, 0, 0,
+          0, 255, 255, 255
+        ]
+      };
+    }
+    return createElement(tagName);
+  };
+});
+
+describe('Default', () => {
+  it('Correct', () => {
+    let canv = document.createElement('canvas');
+    let classCanvas = new Binary2D(canv);
+    expect(classCanvas.getBinaryData()).toEqual([1, 1, 0, 1]);
+  })
 })
