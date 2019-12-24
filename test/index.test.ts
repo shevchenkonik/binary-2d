8e63184096ec9e246054af9a51ebd93aee1e8219
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-12-24 12:40:39 +0300

    Test on undefined case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,31 +1,61 @@
 import Binary2D from '../src'
 
-beforeEach(() => {
-  let createElement = document.createElement.bind(document);
-  document.createElement = (tagName: string) => {
-    if (tagName === 'canvas') {
-      return {
-        getContext: () => ({}),
-        measureText: () => ({}),
-        localName: 'canvas',
-        width: 2,
-        height: 2,
-        data: [
-          0, 255, 255, 255,
-          0, 255, 255, 255,
-          0, 0, 0, 0,
-          0, 255, 255, 255
-        ]
-      };
-    }
-    return createElement(tagName);
-  };
-});
+describe('Default with Canvas', () => {
+  beforeEach(() => {
+    let createElement = document.createElement.bind(document);
+    document.createElement = (tagName: string) => {
+      if (tagName === 'canvas') {
+        return {
+          getContext: () => ({}),
+          measureText: () => ({}),
+          localName: 'canvas',
+          width: 2,
+          height: 2,
+          data: [
+            0, 255, 255, 255,
+            0, 255, 255, 255,
+            0, 0, 0, 0,
+            0, 255, 255, 255
+          ]
+        };
+      }
+      return createElement(tagName);
+    };
+  });
 
-describe('Default', () => {
-  it('Correct', () => {
+  it('Condition', () => {
     let canv = document.createElement('canvas');
     let classCanvas = new Binary2D(canv);
     expect(classCanvas.getBinaryData()).toEqual([1, 1, 0, 1]);
+  })
+})
+
+describe('Correct with Div', () => {
+  beforeEach(() => {
+    let createElement = document.createElement.bind(document);
+    document.createElement = (tagName: string) => {
+      if (tagName === 'canvas') {
+        return {
+          getContext: () => ({}),
+          measureText: () => ({}),
+          localName: 'div',
+          width: 2,
+          height: 2,
+          data: [
+            0, 255, 255, 255,
+            0, 255, 255, 255,
+            0, 0, 0, 0,
+            0, 255, 255, 255
+          ]
+        };
+      }
+      return createElement(tagName);
+    };
+  });
+
+  it('Condition', () => {
+    let canv = document.createElement('canvas');
+    let classCanvas = new Binary2D(canv);
+    expect(classCanvas.getBinaryData()).toBeUndefined();
   })
 })
